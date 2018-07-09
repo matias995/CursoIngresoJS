1d2c213dfbfcab28591ef7fbb982778e58eaeeaa
matias995 <40811430+matias995@users.noreply.github.com>
2018-07-09 20:17:44 -0300

    Update jsEntradaSalida-2.js

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,23 +2,8 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	//var person = prompt("Please enter your name", "Harry Potter");
-	//comentario de una linea;
-	/*
-
-
-	sirve para tomar apuntes*/;
 	var nombre;
-	//"comentario";
-	//alert (nombre);
-	//nombre="Matias";
-	nombre=prompt("ingrese nombre");
-	alert ("su nombre es :"+ nombre);
-	nombre=prompt ("ingrese","natalia natalia");
-	
-
-
-
+	nombre=prompt("aqui escriba su nombre");
+	alert(nombre);
 
 }
-
